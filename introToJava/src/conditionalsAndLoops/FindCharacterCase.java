4af7fe95b0a2e868a512ad886641a43fde062364
amitmungare <amungare27@gmail.com>
2022-03-14 23:45:48 +0530

    FindCharacterCase

---
+++ introToJava/src/conditionalsAndLoops/FindCharacterCase.java
@@ -0,0 +1,22 @@
+package conditionalsAndLoops;
+
+import java.util.Scanner;
+
+public class FindCharacterCase {
+
+	public static void main(String[] args) {
+		
+		Scanner sc =  new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		
+		if(c>=65 && c<=90) {
+			System.out.println("1");
+		}else if(c>=97 && c<=122) {
+			System.out.println("0");
+		}else {
+			System.out.println("-1");
+		}
+		
+	}
+
+}
