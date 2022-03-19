7a5a6f824446c32168d473e45b151a63b4164ac0
amitmungare <amungare27@gmail.com>
2022-03-19 22:14:03 +0530

    arrays

---
+++ introToJava/src/arrays/PairSum.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class PairSum {
+	
+	public static int linearSearch(int arr[], int x) {
+        
+		int count=0;
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[i]+arr[j]==x) {
+					count++;
+				}
+			}
+		}
+		return count;
+    }
+
+	public static void main(String[] args) {
+
+		int[] arr= {1, 3, 6, 2, 5, 4, 3, 2, 4};		
+		System.out.println(linearSearch(arr, 7));
+		
+	}
+
+}
