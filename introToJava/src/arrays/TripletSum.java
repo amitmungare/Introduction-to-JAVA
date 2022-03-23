5701f6555b00bb7e0b758927cc980cfa7a107de6
amitmungare <amungare27@gmail.com>
2022-03-23 20:43:10 +0530

    arrays

---
+++ introToJava/src/arrays/TripletSum.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class TripletSum {
+
+    public static int findTriplet(int[] arr, int x) {
+    	
+    	int count=0;
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=i+1; j<arr.length; j++) {
+				for(int k=j+1; k<arr.length; k++) {
+					if(arr[i]+arr[j]+arr[k]==x) {
+						count++;
+					}
+				}	
+			}
+		}
+		return count;
+        
+    }
+
+	public static void main(String[] args) {
+		int[] arr= {1, 2, 3 ,4, 5, 6, 7};		
+		System.out.println(findTriplet(arr, 12));
+	}
+
+}
