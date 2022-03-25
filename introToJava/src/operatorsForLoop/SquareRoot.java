b66cebb5f536b6a8585128d34711e90cf2edc6ad
amitmungare <amungare27@gmail.com>
2022-03-25 23:46:21 +0530

    loops

---
+++ introToJava/src/operatorsForLoop/SquareRoot.java
@@ -0,0 +1,16 @@
+package operatorsForLoop;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		
+		int n=10;
+		int sqr=0;
+		for(int i=0; i*i<=n; i++) {
+			sqr=i;
+		}
+		System.out.println(sqr);
+
+	}
+
+}
