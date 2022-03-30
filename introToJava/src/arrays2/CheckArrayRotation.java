b0cc35da961f1de7356cf01d772ea7caf2441a04
amitmungare <amungare27@gmail.com>
2022-03-30 21:17:40 +0530

    array

---
+++ introToJava/src/arrays2/CheckArrayRotation.java
@@ -0,0 +1,22 @@
+package arrays2;
+
+public class CheckArrayRotation {
+	
+	public static int arrayRotateCheck(int[] arr){
+    	
+        for(int i=0; i<arr.length-1; i++) {
+        	if(arr[i]>arr[i+1]) {
+        		return i+1;
+        	}
+        }
+        return 0;
+		
+    }
+
+	public static void main(String[] args) {
+		int arr[] = {5, 6, 1, 2, 3, 4};
+		System.out.println(arrayRotateCheck(arr));
+
+	}
+
+}
